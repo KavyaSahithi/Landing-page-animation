ab48d876a3a85016966671dde36c86a698cdacd4
Kavya Sahithi <kavya.lily@gmail.com>
2020-12-24 12:43:44 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const tl = gsap.timeline({ defaults: { ease: 'power1.out' } })
+
+tl.to('.text', { y: '0%', duration: 1.25, stagger: 0.25 })
+tl.to('.slider', { y: '-100%', duration: 3, delay: 1 })
+tl.to('.intro', { y: '-100%', duration: 1 }, '-=2.25')
+tl.fromTo('nav', { opacity: 0 }, { opacity: 1, duration: 1 })
+tl.fromTo('.big-text', { opacity: 0 }, { opacity: 1, duration: 1 }, '-=0.5')
